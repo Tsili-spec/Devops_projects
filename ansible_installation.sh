81bb105e58a02b3ae53c55e1104a962f95e42342
Ubuntu <ubuntu@ip-172-31-30-48.ec2.internal>
2024-07-20 10:29:58 +0000

    Ansible installation script

---
+++ ansible_installation.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt-add-repository ppa:ansible/ansible #installing ansible repo
+sudo apt install ansible -y #installing ansible from ansible repo
+#good pratice is to create an ansible user but here we are not doing it because we want ansible and terraform to have same user
